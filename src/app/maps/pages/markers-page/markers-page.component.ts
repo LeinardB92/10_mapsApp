3a995042841f3e6a62a7e23c7711eff0528dd8b1
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2023-05-18 13:36:11 -0600

    Agregar y eliminar marcadores de una lista con colores

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,6 +1,13 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { LngLat, Map, Marker } from 'mapbox-gl';
 
+
+interface MarkerAndColor {
+  color: string;
+  marker: Marker;
+}
+
+
 @Component({
   selector: 'app-markers-page',
   templateUrl: './markers-page.component.html',
@@ -10,8 +17,11 @@ export class MarkersPageComponent implements AfterViewInit{
 
   @ViewChild('map') divMap?: ElementRef;
 
+  public markers: MarkerAndColor[] = [];
+
   public map?: Map;
   public currentLngLat: LngLat = new LngLat(-74.10380784179445, 4.651165392795477);
+
 
   ngAfterViewInit(): void {
 
@@ -24,12 +34,44 @@ export class MarkersPageComponent implements AfterViewInit{
       zoom: 13, // starting zoom
     });
 
-    const markerHtml = document.createElement('div');
-    markerHtml.innerHTML = 'Soy un cacahuate'
+    // Ejemplo básico de como se coloca un marcador.
+
+    // const markerHtml = document.createElement('div');
+    // markerHtml.innerHTML = 'Soy un cacahuate'
+
+    // const marker = new Marker({
+    //   // color: 'red'
+    //   element: markerHtml
+    // }).setLngLat(this.currentLngLat).addTo(this.map);
+  }
+
+
+  createMarker( lngLat: LngLat, color: string ) {
+    if ( !this.map ) return;
 
     const marker = new Marker({
-      // color: 'red'
-      element: markerHtml
-    }).setLngLat(this.currentLngLat).addTo(this.map);
+      color: color,
+      draggable: true
+    })
+      .setLngLat( lngLat )
+      .addTo( this.map );
+
+      this.markers.push({ color, marker, });
+  }
+
+
+  addMarker() {
+    if ( !this.map ) return;
+
+    const color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));
+    const lngLat = this.map.getCenter();
+
+    this.createMarker( lngLat, color );
+  }
+
+
+  deleteMarker( index: number ) {
+    this.markers[index].marker.remove();
+    this.markers.splice( index, 1 );
   }
 }

37ead8007f0d9afee75809689f99aa176acd33db
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2023-05-19 14:09:24 -0600

    Standalone como componente reutilizable

--- src/app/alone/pages/alone-page/alone-page.component.ts
+++ src/app/alone/pages/alone-page/alone-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SideMenuComponent } from '../../components/side-menu/side-menu.component';
+import { CounterAloneComponent } from '../../components/counter-alone/counter-alone.component';
 
 @Component({
   selector: 'app-alone-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SideMenuComponent, CounterAloneComponent],
   templateUrl: './alone-page.component.html',
   styleUrls: ['./alone-page.component.css']
 })
